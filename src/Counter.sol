e99cec56dab56c122b79d5f237ea858d34c90cbb
etsvigun <utgarda@gmail.com>
2023-10-08 16:22:35 +0300

    remappings.txt: openzeppelin #2

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+
+// this is how we import stuff with those remappings
+import "@uniswap-v3-core/interfaces/IUniswapV3Factory.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Counter {
     uint256 public number;
